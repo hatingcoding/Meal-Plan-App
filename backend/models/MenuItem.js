8f38bdea3074ba4a7f66bae51e3e3d03db0c5567
hatingcoding <dewtojayden@gmail.com>
2026-04-07 08:30:23 +0000

    Refactorising for MenuItem CRUD

--- backend/models/MenuItem.js
+++ backend/models/MenuItem.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
 const menuItemSchema = new mongoose.Schema({
-name: {type: String, required: true},
-id: {type: String, required: true},
-description: {type: String},
-category: {type: String},
-available: {type: Boolean, default: true});
+    itemname: { type: String, required: true },
+    description: { type: String },
+    category: { type: String },
+    available: { type: Boolean, default: true }});
 
-module.exports = mongoose.model('Task', taskSchema);
-
+module.exports = mongoose.model('MenuItem', menuItemSchema);
