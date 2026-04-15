efd21b67b1e7f31a89e82c971cdbfc1db7d858a3
hatingcoding <dewtojayden@gmail.com>
2026-04-15 22:25:34 +0000

    fix: add missing express router declaration in menuItemRoutes

--- backend/routes/menuItemRoutes.js
+++ backend/routes/menuItemRoutes.js
@@ -1,3 +1,5 @@
+const express = require('express');
+const router = express.Router();
 const {getMenuItem, addMenuItem, updateMenuItem, deleteMenuItem} = require('../controllers/menuItemController');
 
 // Read
